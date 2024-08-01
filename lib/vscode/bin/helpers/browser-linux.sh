c719dec42bc961344ef12fb5d696c0782377b20b
Mohamed.H <mhafeel@computer.com>
2024-08-01 11:56:43 +0000

    init commit

---
+++ lib/vscode/bin/helpers/browser-linux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+VSROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+ROOT="$(dirname "$(dirname "$VSROOT")")"
+
+APP_NAME="code-server"
+VERSION="1.91.1"
+COMMIT="1962f48b7f71772dc2c060dbaa5a6b4c0792a549"
+EXEC_NAME="code-server"
+CLI_SCRIPT="$VSROOT/out/server-cli.js"
+"${NODE_EXEC_PATH:-$ROOT/lib/node}" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
---
+++ lib/vscode/bin/remote-cli/code.cmd
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+set ROOT_DIR=%~dp0..\..\..\..
+set VSROOT_DIR=%~dp0..\..
+call "%ROOT_DIR%\node.exe" "%VSROOT_DIR%\out\server-cli.js" "code-server" "1.91.1" "1962f48b7f71772dc2c060dbaa5a6b4c0792a549" "code-server.cmd" %*
+endlocal
